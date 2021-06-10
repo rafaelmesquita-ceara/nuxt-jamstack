6c8bb09c8423e82eb34bd36196bcdbf88583a8ec
Rafael Mesquita <rafael.brito@adaptivesoft.com.br>
2021-06-10 17:15:50 -0300

    4

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -6,7 +6,7 @@
 <script>
 export default {
   async asyncData({ $content, params }) {
-    const article = await $content('articles', params.slug).fetch()
+    const article = await $content('blog', params.slug).fetch()
 
     return { article }
   },
